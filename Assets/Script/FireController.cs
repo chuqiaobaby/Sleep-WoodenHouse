d879c516e4ce2f44cf3250fed39e2638fce8c19d
Chuqiao <chuqiao1125@gmail.com>
2023-03-31 23:34:54 +1100

    Fire Audio and Script

--- Assets/Script/FireController.cs
+++ Assets/Script/FireController.cs
@@ -5,7 +5,9 @@ public class FireController : MonoBehaviour
     public ParticleSystem fireParticles;
     public ParticleSystem emberParticles;
     public Light fireLight;
+    public AudioSource fireAudioSource;
     public float duration = 5.0f;  // fire die out time
+    private float timer = 0f;
 
     private float startTime;
     private float initialFireEmissionRate;
@@ -18,12 +20,17 @@ public class FireController : MonoBehaviour
         initialFireEmissionRate = fireParticles.emissionRate;
         initialEmberEmissionRate = emberParticles.emissionRate;
         initialIntensity = fireLight.intensity;
+
+        fireAudioSource.Play();
     }
 
     void Update()
     {
+        timer += Time.deltaTime;
+
         float timeElapsed = Time.time - startTime;
         float progress = Mathf.Clamp01(timeElapsed / duration);
+        float volume = Mathf.Lerp(1f, 0f, timer / duration);
 
         // reduce fire particle speed
         float newFireEmissionRate = initialFireEmissionRate * (1 - progress);
@@ -38,5 +45,14 @@ public class FireController : MonoBehaviour
         // reduce light intensity
         float newIntensity = initialIntensity * (1 - progress);
         fireLight.intensity = newIntensity;
+
+        // calculate audio volume
+        fireAudioSource.volume = volume;
+
+        // destory object when time run out
+        if (timer >= duration)
+        {
+            Destroy(gameObject);
+        }
     }
 }
